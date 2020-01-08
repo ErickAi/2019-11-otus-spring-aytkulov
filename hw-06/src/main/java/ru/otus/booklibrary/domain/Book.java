ddebb7a30dbdfecba7c70c99bec43d4c2faf990b
Erick Aytkulov <e.aytkulov@gmail.com>
2020-01-08 04:58:06 +0300

    add: domain with JPA annotation

---
+++ hw-06/src/main/java/ru/otus/booklibrary/domain/Book.java
@@ -0,0 +1,54 @@
+package ru.otus.booklibrary.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "name"})
+@Entity
+@Table(name = "books")
+public class Book implements HasId {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", insertable = false, updatable = false)
+    private Author author;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "book_genre", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private Set<Genre> genres;
+
+    public Book(Integer id, String name, Author author) {
+        this(id, name, author, null);
+        this.name = name;
+        this.author = author;
+    }
+
+    public Set<Genre> getGenres() {
+        return genres != null ? genres : new HashSet<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{(id=" + id + ") \'" + name + '\'' +
+                ", author: " + author.getName() +
+                ", genres: " + getGenres().stream().map(Genre::getName).collect(Collectors.toList()).toString() +
+                '}';
+    }
+}
