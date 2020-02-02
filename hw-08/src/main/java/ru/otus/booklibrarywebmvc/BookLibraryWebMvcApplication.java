2d83260a1bb570621b2dfd68d6f44868c41c36ba
Erick Aytkulov <e.aytkulov@gmail.com>
2020-02-02 22:31:18 +0300

    add: basic configuration

---
+++ hw-08/src/main/java/ru/otus/booklibrarywebmvc/BookLibraryWebMvcApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.booklibrarywebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibraryWebMvcApplication.class, args);
+    }
+
+}
