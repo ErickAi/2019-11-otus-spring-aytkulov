1de1e2759fccdfd1c16b3e401a88a9fb92396854
Erick Aytkulov <aytkulov@iac.spb.ru>
2020-02-18 14:26:07 +0300

    upd: hw-08 rename package

--- hw-08/src/main/java/ru/otus/booklibrarymongodb/BookLibraryMongoDb.java
+++ hw-08/src/main/java/ru/otus/booklibrarymongodb/BookLibraryMongoDb.java
@@ -1,4 +1,4 @@
-package ru.otus.booklibrarywebmvc;
+package ru.otus.booklibrarymongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
