63d96bab1ab0b6979b5293ef248f2c8d989d74af
Erick Aytkulov <e.aytkulov@gmail.com>
2020-07-01 16:54:44 +0300

    hw-13: fix tests @EnableGlobalMethodSecurity config

--- hw-13-book-library-acl/back-acl/src/test/java/ru/otus/booklibrary/SecurityTestConfig.java
+++ hw-13-book-library-acl/back-acl/src/test/java/ru/otus/booklibrary/SecurityTestConfig.java
@@ -3,6 +3,7 @@ package ru.otus.booklibrary;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import ru.otus.booklibrary.domain.Role;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 import java.util.Set;
 
 @TestConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityTestConfig {
 
     @Bean
@@ -22,7 +24,7 @@ public class SecurityTestConfig {
         User admin = new User(1L, "Admin Admin", "a@a.a", "password",
                 Set.of(Role.ROLE_ADMIN, Role.ROLE_USER));
         User user = new User(2L, "User User", "u@u.u", "password",
-                Set.of(Role.ROLE_ADMIN, Role.ROLE_USER));
+                Set.of(Role.ROLE_USER));
 
         return new InMemoryUserDetailsManager(Arrays.asList(new AuthUser(admin), new AuthUser(user)));
     }
